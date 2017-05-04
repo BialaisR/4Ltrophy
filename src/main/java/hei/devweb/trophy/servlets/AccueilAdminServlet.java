58bfa06cbd41393478f4c428e4549a37926bff7b
Baptiste Beghin <baptistebeghin@hotmail.fr>
2017-05-04 11:27:08 +0200

    Création du filtre administrateur et admin/actu
    Maintenant -> créer une servlet pour chaque page admin
    Il faut aussi créer un encart "connecté - deconnection"

---
+++ src/main/java/hei/devweb/trophy/servlets/AccueilAdminServlet.java
@@ -0,0 +1,38 @@
+package hei.devweb.trophy.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import hei.devweb.trophy.services.ActualitesService;
+import hei.devweb.trophy.services.EvenementsService;
+
+
+@WebServlet("/admin/accueil")
+public class AccueilAdminServlet extends AbstractGenericServlet{
+
+	private static final long serialVersionUID = -3101071491815001778L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		TemplateEngine templateEngine = this.createTemplateEngine(req);
+		WebContext context = new WebContext(req, resp, getServletContext());
+		context.setVariable("actualites",ActualitesService.getInstance().listActualites());
+		context.setVariable("evenements",EvenementsService.getInstance().listEvenements());
+		
+		templateEngine.process("accueilAdmin", context, resp.getWriter());
+		
+	}
+	
+	
+
+	
+
+}
--- src/main/java/hei/devweb/trophy/servlets/ConnexionServlet.java
+++ src/main/java/hei/devweb/trophy/servlets/ConnexionServlet.java
@@ -1,30 +1,55 @@
 package hei.devweb.trophy.servlets;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
+
 
 
 @WebServlet("/connexion")
-public class ConnexionServlet extends AbstractGenericServlet{
-
-	private static final long serialVersionUID = -3101071491815001778L;
-
+public class ConnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Map<String,String> adminAutorise;
+	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setCharacterEncoding("UTF-8");
-		TemplateEngine templateEngine = this.createTemplateEngine(req);
-		WebContext context = new WebContext(req, resp, getServletContext());
-		
-		templateEngine.process("connexion", context, resp.getWriter());
-		
+	public void init() throws ServletException {
+		adminAutorise=new HashMap<>();
+		adminAutorise.put("admin@hei.fr", "mdp");
+				
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idadminConnecte = (String) request.getSession().getAttribute("adminConnecte");
+		if (idadminConnecte == null || "".equals(idadminConnecte)) {
+			RequestDispatcher view = request.getRequestDispatcher("WEB-INF/connexion.html");
+			view.forward(request, response);
+			
+		} else {
+			response.sendRedirect("admin/accueil");
+			//mettre admin/accueil dès que le mapping des classes admin aura été fait
+		}
+	
 	}
 	
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String identifiantSaisi = request.getParameter("email");
+		String motDePasseSaisi = request.getParameter("key");
+			if(adminAutorise.containsKey(identifiantSaisi) && motDePasseSaisi=="mdp");
+			{
+				request.getSession().setAttribute("adminConnecte",identifiantSaisi);
+			}
+			response.sendRedirect("connexion");
+	
+}
 }
