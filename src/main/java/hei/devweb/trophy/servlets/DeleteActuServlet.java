411358a05b0c309efe5c1d3b7fbd62ce5532e3bf
Baptiste Beghin <baptistebeghin@hotmail.fr>
2017-05-17 17:30:08 +0200

    Servlets de suppression

--- src/main/java/hei/devweb/trophy/servlets/DeleteActuServlet.java
+++ src/main/java/hei/devweb/trophy/servlets/DeleteActuServlet.java
@@ -1,6 +1,7 @@
 package hei.devweb.trophy.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,23 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import hei.devweb.trophy.services.ActualitesService;
 
-/* Les servlets permettent de relier notre back-end avec notre front-end 
- * et définissent les méthodes qui seront utilisées sur cette page
- */
-
-@WebServlet("/admin/send/actu") // mapping de la servlet (url)
-public class SendActuServlet extends AbstractGenericServlet{
+@WebServlet("/admin/delete/actu") // mapping de la servlet (url)
+public class DeleteActuServlet extends AbstractGenericServlet{
 
 	private static final long serialVersionUID = -3101071491815001778L;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String titreActu = req.getParameter("titreActu");
-		String actu = req.getParameter("actu");
-		ActualitesService.getInstance().addActualites(null, titreActu, actu); // on supprime une actualité
+		Integer idActu = Integer.parseInt(req.getParameter("idActu"));
+		ActualitesService.getInstance().addActualites(idActu, null, null); // on ajoute une nouvelle actualité
 		
 		resp.sendRedirect("../accueil"); // redirection vers l'accueil
 		
 	}
-	
 }
